4373b05c31c7553206f01a32451873ae4c44a6c7
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-02-14 16:17:29 -0600

    Use ds.qr_code instead of script function

--- create_qr_code.py
+++ create_qr_code.py
@@ -3,41 +3,16 @@
 Create a QR code from a string.
 """
 
-from typing import NoReturn
 from pathlib import Path
-import pyqrcode as pq
+
+import datasense as ds
 
 
 def main():
     qr_code_string = input("Enter QR code string: ")
-    qr_code_path = input("Enter QR code path  : ")
+    qr_code_path = Path(input("Enter QR code path  : "))
     # qr_code_type = input('Which file format ["svg", "png"]? ')
-    create_code(qr_code_string=qr_code_string, qr_code_path=qr_code_path)
-
-
-def create_code(qr_code_string: str, qr_code_path: str) -> NoReturn:
-    """
-    Create a QR code and save as .svg and .png.
-
-    Parameters
-    ----------
-    qr_code_string : str
-        Text for the QR code
-    qr_code_path : str
-        Text for the path
-
-    Example
-    -------
-    >>> python create_qr_code.py
-    >>> Enter QR code string: https://github.com/gillespilon
-    >>> Enter QR code path  : ~/downloads/mygithuburl
-    """
-    pq.create(content=qr_code_string).svg(
-        Path(qr_code_path).with_suffix(".svg"), scale=4
-    )
-    pq.create(content=qr_code_string).png(
-        Path(qr_code_path).with_suffix(".png"), scale=4
-    )
+    ds.qr_code(qr_code_string=qr_code_string, qr_code_path=qr_code_path)
 
 
 if __name__ == "__main__":
