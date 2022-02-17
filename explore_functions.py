326c76daff0180c75c89ad0ad5c0b9798d23bb54
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-02-17 17:20:19 -0600

    Add html output

--- explore_functions.py
+++ explore_functions.py
@@ -8,6 +8,19 @@ from typing import List, Tuple, Union
 import datasense as ds
 
 
+def main():
+    output_url = "explore_functions.html"
+    header_title = "Explore functions"
+    header_id = "explore-functions"
+    functions = [function_name_syntax, function_name]
+    original_stdout = ds.html_begin(
+        output_url=output_url, header_title=header_title, header_id=header_id
+    )
+    for function in functions:
+        ds.explore_functions(function=function)
+    ds.html_end(original_stdout=original_stdout, output_url=output_url)
+
+
 def function_name_syntax(
     positional_only_parameters,
     /,
@@ -33,12 +46,6 @@ def function_name(
     j: Union[List[str], Tuple[float, int], float, int, None] = None,
 ):
     pass
-
-
-def main():
-    functions = [function_name_syntax, function_name]
-    for function in functions:
-        ds.explore_functions(function=function)
 
 
 if __name__ == '__main__':
