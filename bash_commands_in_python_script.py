7197b84dc73470138c1e2599967adbc35a57ad63
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-02-19 15:03:30 -0600

    Delete list of directories

--- bash_commands_in_python_script.py
+++ bash_commands_in_python_script.py
@@ -9,9 +9,12 @@ import datasense as ds
 
 
 def main():
-    directories = ["directory_13", "directory_69"]
+    directories_new = ["directory_07", "directory_13", "directory_69"]
+    directories_old = ["directory_13", "directory_69"]
     # create a list of empty directories
-    ds.create_directory(directories=directories)
+    ds.create_directory(directories=directories_new)
+    # delete list of directories
+    ds.delete_directory(directories=directories_old)
 
 
 if __name__ == '__main__':
