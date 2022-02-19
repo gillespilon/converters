e860060bddc3d938fffe4bf57d5360d021e9002d
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-02-19 16:22:35 -0600

    Create rename directories

--- bash_commands_in_python_script.py
+++ bash_commands_in_python_script.py
@@ -9,12 +9,18 @@ import datasense as ds
 
 
 def main():
-    directories_new = ["directory_07", "directory_13", "directory_69"]
-    directories_old = ["directory_13", "directory_69"]
+    directories_create = ["directory_07", "directory_13", "directory_69"]
+    directories_delete = ["directory_13", "directory_69"]
+    directories_old_name = ["directory_07"]
+    directories_new_name = ["directory_77"]
     # create a list of empty directories
-    ds.create_directory(directories=directories_new)
+    ds.create_directory(directories=directories_create)
     # delete list of directories
-    ds.delete_directory(directories=directories_old)
+    ds.delete_directory(directories=directories_delete)
+    # rename directories
+    ds.rename_directory(
+        sources=directories_old_name, destinations=directories_new_name
+    )
 
 
 if __name__ == '__main__':
