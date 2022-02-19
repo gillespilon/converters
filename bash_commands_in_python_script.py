f8ef3d3d5447106165bd6b3acf09e464fecb7d7f
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-02-19 11:30:47 -0600

    Create bash_commands_in_python_script.py

---
+++ bash_commands_in_python_script.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+"""
+Using bash commands in Python script.
+"""
+
+from pathlib import Path
+import time
+
+import datasense as ds
+import pandas as pd
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
