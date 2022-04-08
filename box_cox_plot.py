aa6abf2d0cff81301022ebb325c5024e68cfa183
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-04-08 11:01:39 -0500

    Change colours of lines

--- box_cox_plot.py
+++ box_cox_plot.py
@@ -19,6 +19,7 @@ import datasense as ds
 
 def main():
     start_time = time.perf_counter()
+    colour1, colour2 = "#0077bb", "#33bbee"
     output_url = 'box_cox_plt.html'
     header_title = 'Box-Cox Plot'
     header_id = 'box_cox_plot'
@@ -40,8 +41,9 @@ def main():
     # create a tuple of two ndarrays
     # fix this so the arrays are series and probl is replace with fig, ax
     stats.boxcox_normplot(x=s, la=la, lb=lb, plot=ax)
+    ax.get_lines()[0].set(color=colour1, marker='.', markersize=4)
     boxcox, lmax_mle, (min_ci, max_ci) = stats.boxcox(x=s, alpha=0.05)
-    ax.axvline(lmax_mle, color='r', label=f'lmax_mle = {lmax_mle:.3f}')
+    ax.axvline(lmax_mle, color=colour1, label=f'lmax_mle = {lmax_mle:.3f}')
     ax.legend(frameon=False)
     print(f"lmax_mle     : {lmax_mle:.3f}")
     print(f"min_ci       : {min_ci  :.3f}")
