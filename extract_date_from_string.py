4fd246ca24f60fa5b2858eed0c9c4d35ac0b3159
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-06-21 09:41:33 -0500

    Refactor for clarity

--- extract_date_from_string.py
+++ extract_date_from_string.py
@@ -1,21 +1,57 @@
 #! /usr/bin/env python3
 """
-Extract a date from a string.:w
+Use Python re to extract a date from a list of strings.
+
+The code is entirely mine, not copied from anywhere.
+
+The subject is in response to someone's query.
 """
 
 import re
 
 
 def main():
-    text = "Hello world 06/08/1956 to new life."
-    regex = r"\d{1,2}\/\d{1,2}\/\d{2,4}"
-    extracted_date = re.search(regex, text)
-    print(extracted_date.group(0))
-    print()
-    text = "Hello world 1956-06-08 to new life."
-    regex = r"\d{2,4}-\d{1,2}-\d{1,2}"
-    extracted_date = re.search(regex, text)
-    print(extracted_date.group(0))
+    items = [
+        "Hello 06/08/1956 date.",
+        "Hello 1956-06-08 date."
+    ]
+    # items = (
+    #     "Hello 06/08/1956 date.",
+    #     "Hello 1956-06-08 date."
+    # )
+    regex = re.compile(pattern=r"\d{1,2}\/\d{1,2}\/\d{2,4}")
+    matches = [item for item in items if regex.search(string=item)]
+    print(matches)
+    # ['Hello 06/08/1956 date.']
+    matches = [
+        regex.search(string=item).group(0) for item in items
+        if regex.search(string=item)
+    ]
+    print(matches)
+    # ['06/08/1956']
+    regex = re.compile(pattern=r"\d{2,4}-\d{1,2}-\d{1,2}")
+    matches = [item for item in items if regex.search(string=item)]
+    print(matches)
+    # ['Hello 1956-06-08 date.']
+    matches = [
+        regex.search(string=item).group(0) for item in items
+        if regex.search(string=item)
+    ]
+    print(matches)
+    # ['1956-06-08']
+    regex = re.compile(
+        pattern=r"\d{2,4}-\d{1,2}-\d{1,2}|\d{1,2}\/\d{1,2}\/\d{2,4}"
+    )
+    matches = [item for item in items if regex.search(string=item)]
+    print(matches)
+    # ['Hello 06/08/1956 date.', 'Hello 1956-06-08 date.']
+    matches = [
+        regex.search(string=item).group(0) for item in items
+        if regex.search(string=item)
+    ]
+    print(matches)
+    # ['06/08/1956', '1956-06-08']
+
 
 if __name__ == "__main__":
     main()
