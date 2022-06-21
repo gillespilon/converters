c475625f909db484f8073c3ecdd66c29e98c4b5e
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-06-21 13:33:08 -0500

    Add code for optional characters

--- extract_text_from_list_tuple.py
+++ extract_text_from_list_tuple.py
@@ -16,6 +16,7 @@ def main():
     pattern1 = r"abc"
     pattern2 = r"abcd*e*f*g*"
     pattern3 = r"[^a][b-c]"
+    pattern4 = r"[^a][b-c]d?e?"
     regex = re.compile(pattern=pattern1)
     matches = [item for item in items if regex.fullmatch(string=item)]
     print(matches)
@@ -45,6 +46,16 @@ def main():
     ]
     print(matches)
     # ['bc', 'bc', 'bc']
+    regex = re.compile(pattern=pattern4)
+    matches = [item for item in items if regex.search(string=item)]
+    print(matches)
+    # ['abcdefg', 'abcde', 'abc']
+    matches = [
+        regex.search(string=item).group(0) for item in items
+        if regex.search(string=item)
+    ]
+    print(matches)
+    # ['bcde', 'bcde', 'bc']
 
 
 if __name__ == "__main__":
