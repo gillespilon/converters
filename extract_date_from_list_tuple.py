7a2ff248d33d6a131b4fb9a8da9c5196f7a80077
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-06-23 15:31:40 -0500

    Refactor to use word boundaries, ensure correctness

--- extract_date_from_list_tuple.py
+++ extract_date_from_list_tuple.py
@@ -12,45 +12,70 @@ import re
 
 def main():
     items = [
-        "Hello 06/08/1956 date.",
-        "Hello 1956-06-08 date."
+        "A 06/08/1956 A 01/14/1968 A",
+        "B 1956-06-08 B 1968-01-14 B",
+        "C 1956-06-08 C 01/14/1968 C"
     ]
     # items = (
-    #     "Hello 06/08/1956 date.",
-    #     "Hello 1956-06-08 date."
+    #     "A 06/08/1956 A 01/14/1968 A",
+    #     "B 1956-06-08 B 1968-01-14 B",
+    #     "C 1956-06-08 C 01/14/1968 C"
     # )
-    regex = re.compile(pattern=r"\d{1,2}\/\d{1,2}\/\d{2,4}")
-    matches = [item for item in items if regex.search(string=item)]
+    pattern1 = r"\b\d{1,2}\/\d{1,2}\/\d{2,4}\b"
+    pattern2 = r"\b\d{2,4}-\d{1,2}-\d{1,2}\b"
+    regex = re.compile(pattern=pattern1)
+    matches = [item for item in items if regex.findall(string=item)]
     print(matches)
-    # ['Hello 06/08/1956 date.']
+    # ['A 06/08/1956 A 01/14/1968 A', 'C 1956-06-08 C 01/14/1968 C']
     matches = [
-        regex.search(string=item).group(0) for item in items
-        if regex.search(string=item)
+        regex.findall(string=item) for item in items
+        if regex.findall(string=item)
     ]
     print(matches)
-    # ['06/08/1956']
-    regex = re.compile(pattern=r"\d{2,4}-\d{1,2}-\d{1,2}")
-    matches = [item for item in items if regex.search(string=item)]
+    # [['06/08/1956', '01/14/1968'], ['01/14/1968']]
+    matches = [item for sublist in matches for item in sublist]
     print(matches)
-    # ['Hello 1956-06-08 date.']
+    # ['06/08/1956', '01/14/1968', '01/14/1968']
+    regex = re.compile(pattern=pattern2)
+    matches = [item for item in items if regex.findall(string=item)]
+    print(matches)
+    # ['B 1956-06-08 B 1968-01-14 B', 'C 1956-06-08 C 01/14/1968 C']
     matches = [
-        regex.search(string=item).group(0) for item in items
-        if regex.search(string=item)
+        regex.findall(string=item) for item in items
+        if regex.findall(string=item)
     ]
     print(matches)
-    # ['1956-06-08']
+    # [['1956-06-08', '1968-01-14'], ['1956-06-08']]
+    matches = [item for sublist in matches for item in sublist]
+    print(matches)
+    # ['1956-06-08', '1968-01-14', '1956-06-08']
     regex = re.compile(
-        pattern=r"\d{2,4}-\d{1,2}-\d{1,2}|\d{1,2}\/\d{1,2}\/\d{2,4}"
+        pattern=pattern1 + "|" + pattern2
     )
-    matches = [item for item in items if regex.search(string=item)]
+    matches = [item for item in items if regex.findall(string=item)]
     print(matches)
-    # ['Hello 06/08/1956 date.', 'Hello 1956-06-08 date.']
+    # [
+    #     'A 06/08/1956 A 01/14/1968 A',
+    #     'B 1956-06-08 B 1968-01-14 B',
+    #     'C 1956-06-08 C 01/14/1968 C'
+    # ]
     matches = [
-        regex.search(string=item).group(0) for item in items
-        if regex.search(string=item)
+        regex.findall(string=item) for item in items
+        if regex.findall(string=item)
     ]
     print(matches)
-    # ['06/08/1956', '1956-06-08']
+    # [
+    #     ['06/08/1956', '01/14/1968'],
+    #     ['1956-06-08', '1968-01-14'],
+    #     ['1956-06-08', '01/14/1968']
+    # ]
+    matches = [item for sublist in matches for item in sublist]
+    print(matches)
+    # [
+    #     '06/08/1956', '01/14/1968',
+    #     '1956-06-08', '1968-01-14',
+    #     '1956-06-08', '01/14/1968'
+    # ]
 
 
 if __name__ == "__main__":
