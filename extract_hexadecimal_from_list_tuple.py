f9d6e44c3a49223079187612f84da7379ccc37ae
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-06-27 09:02:41 -0500

    Create extract_hexadecimal_from_list_tuple.py

---
+++ extract_hexadecimal_from_list_tuple.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+"""
+Use Python re to match hexadecimal strings from a list or tuple of strings.
+"""
+
+import re
+
+
+def main():
+    items = [
+        "Text ffffff + 0xffffff & 0Xffffff.",
+        "ffffff", "0xffffff", "0Xffffff"
+    ]
+    # items = (
+    #     "Text ffffff + 0xffffff & 0Xffffff.",
+    #     "ffffff", "0xffffff", "0Xffffff"
+    # )
+    pattern = r"\b[0|(xX)]?[a-fA-FxX0-9]{6,8}\b"
+    regex = re.compile(pattern=pattern)
+    matches = [item for item in items if regex.findall(string=item)]
+    print(matches)
+    # ['Text ffffff + 0xffffff & 0Xffffff.', 'ffffff', '0xffffff', '0Xffffff']
+    matches = [
+        regex.findall(string=item) for item in items
+        if regex.findall(string=item)
+    ]
+    print(matches)
+    # [
+    #     ['ffffff', '0xffffff', '0Xffffff'],
+    #     ['ffffff'], ['0xffffff'], ['0Xffffff']
+    #    ]
+    matches = [item for sublist in matches for item in sublist]
+    print(matches)
+    # ['ffffff', '0xffffff', '0Xffffff', 'ffffff', '0xffffff', '0Xffffff']
+
+
+if __name__ == "__main__":
+    main()
