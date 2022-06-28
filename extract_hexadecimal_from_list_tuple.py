1c2997833e94a2b059257f244d787bee4ddbe7c4
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-06-28 06:56:49 -0500

    Refactor search v findall

--- extract_hexadecimal_from_list_tuple.py
+++ extract_hexadecimal_from_list_tuple.py
@@ -17,12 +17,12 @@ def main():
     # )
     pattern = r"\b[0|(xX)]?[a-fA-FxX0-9]{6,8}\b"
     regex = re.compile(pattern=pattern)
-    matches = [item for item in items if regex.findall(string=item)]
+    matches = [item for item in items if regex.search(string=item)]
     print(matches)
     # ['Text ffffff + 0xffffff & 0Xffffff.', 'ffffff', '0xffffff', '0Xffffff']
     matches = [
         regex.findall(string=item) for item in items
-        if regex.findall(string=item)
+        if regex.search(string=item)
     ]
     print(matches)
     # [
