fe3113fea15691df4c83349042deb6d19673520e
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-12-06 16:32:33 -0600

    Create initial delete_columns.py

---
+++ delete_columns.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+"""
+Code to delete columns based on various criteria.
+"""
+
+from pathlib import Path
+import time
+
+import datasense as ds
+import pandas as pd
+import numpy as np
+
+
+def main():
+    output_url = "delete_columns.html"
+    header_title = "Delete columns"
+    header_id = "delete-columns"
+    columns_to_check = ["integers", "floats", "text"]
+    empty_items = [np.NaN, pd.NaT, None, ""]
+    start_time = time.perf_counter()
+    original_stdout = ds.html_begin(
+        output_url=output_url,
+        header_title=header_title,
+        header_id=header_id
+    )
+    ds.script_summary(
+        script_path=Path(__file__),
+        action="started at"
+    )
+    stop_time = time.perf_counter()
+    ds.script_summary(
+        script_path=Path(__file__),
+        action="finished at"
+    )
+    ds.report_summary(
+        start_time=start_time,
+        stop_time=stop_time
+    )
+    ds.html_end(
+        original_stdout=original_stdout,
+        output_url=output_url
+    )
+
+
+if __name__ == "__main__":
+    main()
