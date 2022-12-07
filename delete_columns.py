7796c52945bba73938401edd7d41cf899348c39d
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-12-07 12:42:38 -0600

    Delete columns where all elements are missing

--- delete_columns.py
+++ delete_columns.py
@@ -1,9 +1,15 @@
 #! /usr/bin/env python3
 """
 Code to delete columns based on various criteria.
+
+dropna works for pd.NaT, np.NaN, and None. It does not work for "" which
+occurs in csv files from Excel.
+
+isin in combination with loc works for pd.NaT, np.NaN, None, and "".`
 """
 
 from pathlib import Path
+import textwrap
 import time
 
 import datasense as ds
@@ -27,6 +33,54 @@ def main():
         script_path=Path(__file__),
         action="started at"
     )
+    print("dropna")
+    print("------")
+    print()
+    print(
+        "dropna works for pd.NaT, np.NaN, and None. "
+        "It does not work for '' which "
+        "occurs in csv files from Excel."
+    )
+    print()
+    df = pd.DataFrame(
+        dict(
+            integers=[1, 2, 3, 4, 5, 6, 7],
+            floats=[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0],
+            text=["A", "B", "C", "D", "E", "F", "G"],
+            dates=[
+                pd.Timestamp("1956-06-08"), pd.Timestamp("1956-06-08"),
+                pd.Timestamp("1956-06-08"), pd.Timestamp("1956-06-08"),
+                pd.Timestamp("1956-06-08"), pd.Timestamp("1956-06-08"),
+                pd.Timestamp("1956-06-08")
+            ],
+            all_nan=[np.NaN, np.NaN, np.NaN, np.NaN, np.NaN, np.NaN, np.NaN],
+            all_nat=[pd.NaT, pd.NaT, pd.NaT, pd.NaT, pd.NaT, pd.NaT, pd.NaT],
+            all_none=[None, None, None, None, None, None, None],
+            all_space=["", "", "", "", "", "", ""],
+            nan_space=[np.NaN, "", np.NaN, np.NaN, np.NaN, np.NaN, np.NaN],
+            nan_none=[np.NaN, None, np.NaN, np.NaN, None, np.NaN, None],
+            mixed=[None, np.NaN, pd.NaT, pd.NaT, None, np.NaN, pd.NaT]
+        )
+    )
+    print(df)
+    print()
+    print(
+        "Delete columns where all elements are missing. It fails to delete "
+        "columns all_space and nan_space."
+    )
+    print()
+    print(textwrap.dedent("""
+        df = df.dropna(
+            axis="columns",
+            how="all"
+        )
+    """))
+    df = df.dropna(
+        axis="columns",
+        how="all"
+    )
+    print(df)
+    print()
     stop_time = time.perf_counter()
     ds.script_summary(
         script_path=Path(__file__),
