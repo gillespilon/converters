8e01fc5c20a50394056f5a8f4a370951b865f499
Gilles Pilon <gilles.pilon@gillespilon.ca>
2023-01-25 19:33:48 -0600

    Capitalize constants

--- statsmodels_multiple_linear_regression.py
+++ statsmodels_multiple_linear_regression.py
@@ -9,43 +9,43 @@ import pandas as pd
 
 
 def main():
-    output_url = "statsmodels_multiple_linear_regression.html"
-    header_title = "Statsmodels multiple linear regression"
-    header_id = "statsmodels-multiple-linear-regression"
+    OUTPUT_URL = "statsmodels_multiple_linear_regression.html"
+    HEADER_TITLE = "Statsmodels multiple linear regression"
+    HEADER_ID = "statsmodels-multiple-linear-regression"
     original_stdout = ds.html_begin(
-        output_url=output_url,
-        header_title=header_title,
-        header_id=header_id
+        output_url=OUTPUT_URL,
+        header_title=HEADER_TITLE,
+        header_id=HEADER_ID
     )
-    series_year = [
+    SERIES_YEAR = [
         2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017,
         2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016
     ]
-    series_month = [
+    SERIES_MONTH = [
         12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3,
         2, 1
     ]
-    series_interest_rate = [
+    SERIES_INTEREST_RATE = [
         2.75, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.25, 2.25, 2.25, 2, 2, 2, 1.75,
         1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75
     ]
-    series_unemployment_rate = [
+    SERIES_UNEMPLOYMENT_RATE = [
         5.3, 5.3, 5.3, 5.3, 5.4, 5.6, 5.5, 5.5, 5.5, 5.6, 5.7, 5.9, 6, 5.9,
         5.8, 6.1, 6.2, 6.1, 6.1, 6.1, 5.9, 6.2, 6.2, 6.1
     ]
-    series_index_price = [
+    SERIES_INDEX_PRICE = [
         1464, 1394, 1357, 1293, 1256, 1254, 1234, 1195, 1159, 1167, 1130, 1075,
         1047, 965, 943, 958, 971, 949, 884, 866, 876, 822, 704, 719
     ]
-    x_column = ["interest_rate", "unemployment_rate"]
-    y_column = "index_price"
+    X_COLUMN = ["interest_rate", "unemployment_rate"]
+    Y_COLUMN = "index_price"
     df = pd.DataFrame(
         data={
-            "year": series_year,
-            "month": series_month,
-            x_column[0]: series_interest_rate,
-            x_column[1]: series_unemployment_rate,
-            y_column: series_index_price
+            "year": SERIES_YEAR,
+            "month": SERIES_MONTH,
+            X_COLUMN[0]: SERIES_INTEREST_RATE,
+            X_COLUMN[1]: SERIES_UNEMPLOYMENT_RATE,
+            Y_COLUMN: SERIES_INDEX_PRICE
         }
     )
     (
@@ -53,13 +53,13 @@ def main():
         confidence_interval_upper, prediction_interval_lower,
         prediction_interval_upper
     ) = ds.linear_regression(
-        X=df[x_column],
-        y=df[y_column],
+        X=df[X_COLUMN],
+        y=df[Y_COLUMN],
         print_model_summary=True
     )
     ds.html_end(
         original_stdout=original_stdout,
-        output_url=output_url
+        output_url=OUTPUT_URL
     )
 
 
