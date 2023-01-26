84498b160280445a9535ca60a32d07659819871c
Gilles Pilon <gilles.pilon@gillespilon.ca>
2023-01-25 19:29:28 -0600

    Capitalize constants

--- statsmodels_linear_regression.py
+++ statsmodels_linear_regression.py
@@ -9,78 +9,74 @@ import pandas as pd
 
 
 def main():
-    output_url = "statsmodels_linear_regression.html"
-    header_title = "Statsmodels linear regression"
-    header_id = "statsmodels-linear-regression"
-    labellegendci = "Confidence interval"
-    labellegendpi = "Prediction interval"
-    labellegendy2 = "Linear regression"
-    lower_ci_column = "mean_ci_lower"
-    upper_ci_column = "mean_ci_upper"
-    lower_pi_column = "obs_ci_lower"
-    upper_pi_column = "obs_ci_upper"
-    title = "Regression analysis"
-    colour_shading = "#888888"
-    graphname = "y_vs_x.svg"
-    xlabel = "X axis label"
+    OUTPUT_URL = "statsmodels_linear_regression.html"
+    HEADER_TITLE = "Statsmodels linear regression"
+    HEADER_ID = "statsmodels-linear-regression"
+    LABELLEGENDCI = "Confidence interval"
+    LABELLEGENDPI = "Prediction interval"
+    LABELLEGENDY2 = "Linear regression"
+    TITLE = "Regression analysis"
+    COLOUR_SHADING = "#888888"
+    GRAPH_NAME = "y_vs_x.svg"
+    XLABEL = "X axis label"
     ylabel = "Y axis label"
-    labellegendy1 = "Data"
-    figsize = (8, 6)
-    x_column = "x"
-    y_column = "y"
+    LABELLEGENDY1 = "Data"
+    FIGSIZE = (8, 6)
+    X_COLUMN = "x"
+    Y_COLUMN = "y"
     original_stdout = ds.html_begin(
-        output_url=output_url,
-        header_title=header_title,
-        header_id=header_id
+        output_url=OUTPUT_URL,
+        header_title=HEADER_TITLE,
+        header_id=HEADER_ID
     )
     df = pd.DataFrame(
         data={
-            x_column: [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5],
-            y_column: [
+            X_COLUMN: [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5],
+            Y_COLUMN: [
                 8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82,
                 5.68
             ]
         }
-    ).sort_values(by=x_column)
+    ).sort_values(by=X_COLUMN)
     (
         fitted_model, predictions, confidence_interval_lower,
         confidence_interval_upper, prediction_interval_lower,
         prediction_interval_upper
     ) = ds.linear_regression(
-        X=df[x_column],
-        y=df[y_column],
+        X=df[X_COLUMN],
+        y=df[Y_COLUMN],
     )
     ds.style_graph()
     fig, ax = ds.plot_scatter_line_x_y1_y2(
-        X=df[x_column],
-        y1=df[y_column],
+        X=df[X_COLUMN],
+        y1=df[Y_COLUMN],
         y2=predictions,
-        figsize=figsize,
-        labellegendy1=labellegendy1,
-        labellegendy2=labellegendy2
+        figsize=FIGSIZE,
+        labellegendy1=LABELLEGENDY1,
+        labellegendy2=LABELLEGENDY2
     )
     ax.fill_between(
-        df[x_column],
+        df[X_COLUMN],
         y1=confidence_interval_lower,
         y2=confidence_interval_upper,
-        color=colour_shading,
+        color=COLOUR_SHADING,
         alpha=0.4,
-        label=labellegendci
+        label=LABELLEGENDCI
     )
     ax.fill_between(
-        x=df[x_column],
+        x=df[X_COLUMN],
         y1=prediction_interval_lower,
         y2=prediction_interval_upper,
-        color=colour_shading,
+        color=COLOUR_SHADING,
         alpha=0.2,
-        label=labellegendpi
+        label=LABELLEGENDPI
     )
     ax.set_title(
-        label=title,
+        label=TITLE,
         fontsize=15
     )
     ax.set_xlabel(
-        xlabel=xlabel,
+        xlabel=XLABEL,
         fontsize=12
     )
     ax.set_ylabel(
@@ -92,13 +88,13 @@ def main():
         frameon=False
     )
     fig.savefig(
-        fname=graphname,
+        fname=GRAPH_NAME,
         format="svg"
     )
-    ds.html_figure(file_name=graphname)
+    ds.html_figure(file_name=GRAPH_NAME)
     ds.html_end(
         original_stdout=original_stdout,
-        output_url=output_url
+        output_url=OUTPUT_URL
     )
 
 
