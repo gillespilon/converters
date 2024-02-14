65d0929c82f86a173f7c11bef6876ed2842de6bd
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-02-13 22:05:58 -0600

    Add argparse path_or_file_delete_keywords

--- ats_keyword_analysis.py
+++ ats_keyword_analysis.py
@@ -18,18 +18,26 @@ def main():
     chdir(Path(__file__).parent.resolve())  # required for cron
     parser = argparse.ArgumentParser(description="ATS Keyword Analysis")
     parser.add_argument(
-        "-pfr",
-        "--path_or_file_resume",
+        "-pfcv",
+        "--path_or_file_cv",
         default=Path("ats_resume.txt"),
         type=Path,
         required=False,
-        help="path or file of the .txt resume file (default: ats_resume.txt",
+        help="path .txt resume (default: ats_resume.txt",
+    )
+    parser.add_argument(
+        "-pfdk",
+        "--path_or_file_delete_keywords",
+        default=Path("ats_delete_keywords.txt"),
+        type=Path,
+        required=False,
+        help="path .txt delete keywords (default: ats_delete_keywords.txt",
     )
     args = parser.parse_args()
+    delete_keywords_path = args.path_or_file_delete_keywords
+    resume_path = args.path_or_file_cv
     delete_substrings_path = Path("ats_delete_substrings.txt")
-    delete_keywords_path = Path("ats_delete_keywords.txt")
     clean_keywords_path = Path("ats_clean_keywords.txt")
-    resume_path = args.path_or_file_resume
     resume_keywords = [(line.rstrip()) for line in resume_path.open()]
     delete_keywords = [(line.rstrip()) for line in delete_keywords_path.open()]
     # read the substrings to delete file, create list of strings
