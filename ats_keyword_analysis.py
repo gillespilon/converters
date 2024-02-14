90eeb2676e909d36c75d777b66edddc767e4fe9d
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-02-14 16:42:31 -0600

    Add argparse path_or_file_substrings

--- ats_keyword_analysis.py
+++ ats_keyword_analysis.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 """
-Compare a text file of an ATS-friendly resume with a text file of a job
-posting.
+Compare a text file of an ATS-friendly cv with a text file of a job posting.
 """
 
 from pathlib import Path
@@ -20,43 +19,61 @@ def main():
     parser.add_argument(
         "-pfcv",
         "--path_or_file_cv",
-        default=Path("ats_resume.txt"),
+        default=Path("ats_cv.txt"),
         type=Path,
         required=False,
-        help="path .txt resume (default: ats_resume.txt",
+        help="path .txt cv (default: ats_cv.txt",
     )
     parser.add_argument(
         "-pfdk",
-        "--path_or_file_delete_keywords",
-        default=Path("ats_delete_keywords.txt"),
+        "--path_or_file_deletions",
+        default=Path("ats_deletions.txt"),
         type=Path,
         required=False,
-        help="path .txt delete keywords (default: ats_delete_keywords.txt",
+        help="path .txt deletions (default: ats_deletions.txt",
+    )
+    parser.add_argument(
+        "-pfst",
+        "--path_or_file_substrings",
+        default=Path("ats_substrings.txt"),
+        type=Path,
+        required=False,
+        help="path .txt substrings (default: ats_substrings.txt",
     )
     args = parser.parse_args()
-    delete_keywords_path = args.path_or_file_delete_keywords
-    resume_path = args.path_or_file_cv
-    delete_substrings_path = Path("ats_delete_substrings.txt")
-    clean_keywords_path = Path("ats_clean_keywords.txt")
-    resume_keywords = [(line.rstrip()) for line in resume_path.open()]
-    delete_keywords = [(line.rstrip()) for line in delete_keywords_path.open()]
+    path_cv_raw = args.path_or_file_cv
+    path_deletions = args.path_or_file_deletions
+    path_substrings = args.path_or_file_substrings
+    path_clean_cv = Path("ats_cv_clean.txt")
+    cv_raw = [(line.rstrip("\n")) for line in path_cv_raw.open()]
+    print("cv_raw")
+    print(cv_raw)
+    deletions = [(line.rstrip("\n")) for line in path_deletions.open()]
+    print("deletions")
+    print(deletions)
+    substrings = [line.rstrip("\n") for line in path_substrings.open()]
+    print("substrings")
+    print(substrings)
+    # cv_keywords_less_delete_keywords = ds.list_one_list_two_ops(
+    #     list_one=cv_keywords, list_two=delete_keywords, action="list_one"
+    # )
+    # print("unique cv_keywords")
+    # print(cv_keywords_less_delete_keywords)
     # read the substrings to delete file, create list of strings
-    delete_substrings = [
-        line.rstrip("\n") for line in delete_substrings_path.open()
-    ]
-    resume_keywords = ds.list_one_list_two_ops(
-        list_one=resume_keywords, list_two=delete_keywords, action="list_one"
-    )
-    resume_keywords = [
-        text.replace(substring, "")
-        for text in resume_keywords
-        for substring in delete_substrings
-        if substring in text
-    ]
-    print(resume_keywords)
-    with open(clean_keywords_path, "w") as file:
-        for string in resume_keywords:
-            file.write(string + "\n")
+    # cv_keywords = [
+    #     text.replace(substring, "")
+    #     for text in cv_keywords
+    #     for substring in delete_substrings
+    #     if substring in text
+    # ]
+    # cv_keywords_less_substrings = [s.replace(st, "") for s in cv_keywords_less_delete_keywords for st in delete_substrings]
+    # cv_keywords_less_substrings = [s for s in cv_keywords_less_delete_keywords if not any(st in s for st in delete_substrings)]
+    # filtered_text_set = set(cv_keywords) - set(substring for substring in delete_substrings)
+    # cv_keywords_less_substrings = list(filtered_text_set)
+    # print(cv_keywords_less_substrings)
+    # with open(clean_keywords_path, "w") as file:
+    #     for string in cv_keywords_less_substrings:
+    #         file.write(string + "\n")
 
 
 if __name__ == "__main__":
