236210e232ad0282e1a4d1c36c00433278ed01ab
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-02-14 22:04:34 -0600

    Fix substring by using regex

--- ats_keyword_analysis.py
+++ ats_keyword_analysis.py
@@ -6,11 +6,9 @@ Compare a text file of an ATS-friendly cv with a text file of a job posting.
 from pathlib import Path
 from os import chdir
 import argparse
-import time
+import re
 
 import datasense as ds
-import pandas as pd
-import numpy as np
 
 
 def main():
@@ -50,38 +48,23 @@ def main():
     )
     args = parser.parse_args()
     path_cv_raw = args.path_or_file_cv_raw
-    path_clean_cv = args.path_or_file_cv_clean
+    path_cv_clean = args.path_or_file_cv_clean
     path_deletions = args.path_or_file_deletions
-    path_substrings = args.path_or_file_substrings
+    # might not be needed if done with regex
+    # path_substrings = args.path_or_file_substrings
     cv_raw = [(line.rstrip("\n")) for line in path_cv_raw.open()]
-    print("cv_raw")
-    print(cv_raw)
     deletions = [(line.rstrip("\n")) for line in path_deletions.open()]
-    print("deletions")
-    print(deletions)
-    substrings = [line.rstrip("\n") for line in path_substrings.open()]
-    print("substrings")
-    print(substrings)
-    # cv_keywords_less_delete_keywords = ds.list_one_list_two_ops(
-    #     list_one=cv_keywords, list_two=delete_keywords, action="list_one"
-    # )
-    # print("unique cv_keywords")
-    # print(cv_keywords_less_delete_keywords)
-    # read the substrings to delete file, create list of strings
-    # cv_keywords = [
-    #     text.replace(substring, "")
-    #     for text in cv_keywords
-    #     for substring in delete_substrings
-    #     if substring in text
-    # ]
-    # cv_keywords_less_substrings = [s.replace(st, "") for s in cv_keywords_less_delete_keywords for st in delete_substrings]
-    # cv_keywords_less_substrings = [s for s in cv_keywords_less_delete_keywords if not any(st in s for st in delete_substrings)]
-    # filtered_text_set = set(cv_keywords) - set(substring for substring in delete_substrings)
-    # cv_keywords_less_substrings = list(filtered_text_set)
-    # print(cv_keywords_less_substrings)
-    # with open(clean_keywords_path, "w") as file:
-    #     for string in cv_keywords_less_substrings:
-    #         file.write(string + "\n")
+    # might not be needed if done with regex
+    # substrings = [line.rstrip("\n") for line in path_substrings.open()]
+    cv_deletions = ds.list_one_list_two_ops(
+        list_one=cv_raw, list_two=deletions, action="list_one"
+    )
+    cv_substrings = [
+        re.sub(r"^Skills: |^\s{4}\• ", "", st) for st in cv_deletions
+    ]
+    with open(path_cv_clean, "w") as f:
+        for s in cv_substrings:
+            f.write(s + "\n")
 
 
 if __name__ == "__main__":
